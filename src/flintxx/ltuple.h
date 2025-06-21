3a4862a6a9459f3751b51dc8bb66e0cf04c1960e
Edgar Costa <edgarc@mit.edu>
2025-06-20 23:41:23 -0400

    fix include

--- src/flintxx/ltuple.h
+++ src/flintxx/ltuple.h
@@ -18,14 +18,13 @@
 #define FLINTXX_LTUPLE_H
 
 #include "flintxx/mp.h"
-#include "flintxx/tuple.h"
+#include "tuple.h"
 
 #ifndef FLINT_LTUPLE_PLACEHOLDER_NAME
 #define FLINT_LTUPLE_PLACEHOLDER_NAME _
 #endif
 
 #include "expression.h"
-#include "tuple.h"
 
 namespace flint {
 // For lazy get<n>, this operation type is created.
